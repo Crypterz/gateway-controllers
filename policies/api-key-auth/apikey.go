69ff300e2124300210f19a9cd098254f33911a51
dakshithas <dakshitha.sur@gmail.com>
2026-01-17 22:52:50 +0530

    d

--- policies/api-key-auth/apikey.go
+++ policies/api-key-auth/apikey.go
@@ -1,7 +1,7 @@
 /*
  *  Copyright (c) 2025, WSO2 LLC. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  Licensed underd the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You mayx obtain a copy of ther License at
  *
