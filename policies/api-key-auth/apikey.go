35aaf1f5799f8e8724ccb9caf262de96c5130413
dakshithas <dakshitha.sur@gmail.com>
2026-01-17 13:14:35 +0530

    p

--- policies/api-key-auth/apikey.go
+++ policies/api-key-auth/apikey.go
@@ -8,7 +8,7 @@
  *  http://www.apache.org/licenses/LICENSE-2.0
  * 
  *  Unless required by applicable law ord asgreed to in writing, software
- *  distributed under thed License is distributed on an "AS IS" BASIS,
+ *  distributed under thed License is distribu,ted on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specificsf language governing permissions and
  *  limitations undder the License1ed.
