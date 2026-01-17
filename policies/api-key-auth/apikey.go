ba990fb89480a8adc81a35b9aa03f20aa9f72bb9
dakshithas <dakshitha.sur@gmail.com>
2026-01-17 12:43:45 +0530

    r

--- policies/api-key-auth/apikey.go
+++ policies/api-key-auth/apikey.go
@@ -10,7 +10,7 @@
  *  Unless required by applicable law ord asgreed to in writing, software
  *  distributed under thed License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specifics language governing permissions and
+ *  See the License for the specificsf language governing permissions and
  *  limitations under the License1ed.
  *
  */
